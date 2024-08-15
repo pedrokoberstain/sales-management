d339fa3761e3a0f5e220c18826e37c5c47c3c944
pedro <pedrokoberstain2@gmail.com>
2024-08-15 01:23:48 -0400

    fix: Refatorando de nome para name

--- src/main/java/com/smanager/sales_management/entity/Categoria.java
+++ src/main/java/com/smanager/sales_management/entity/Categoria.java
@@ -16,7 +16,7 @@ public class Categoria {
     @Column(name="nome")
     @NotBlank(message = "O campo nome é obrigatório")
     @Length(min = 3, max = 50, message = "O campo nome deve ter entre 3 e 50 caracteres")
-    private String nome;
+    private String name;
 
     public Long getCodigo() {
         return codigo;
@@ -26,12 +26,12 @@ public class Categoria {
         this.codigo = codigo;
     }
 
-    public String getNome() {
-        return nome;
+    public String getName() {
+        return name;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setName(String nome) {
+        this.name = nome;
     }
 
     @Override
@@ -39,11 +39,11 @@ public class Categoria {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Categoria categoria = (Categoria) o;
-        return Objects.equals(codigo, categoria.codigo) && Objects.equals(nome, categoria.nome);
+        return Objects.equals(codigo, categoria.codigo) && Objects.equals(name, categoria.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(codigo, nome);
+        return Objects.hash(codigo, name);
     }
 }
