04008628eea44995f5dbac81613434539aa910cc
pedro <pedrokoberstain2@gmail.com>
2024-08-15 01:26:10 -0400

    feat: Criando a validação de categorias duplicadas no service

--- src/main/java/com/smanager/sales_management/service/CategoriaService.java
+++ src/main/java/com/smanager/sales_management/service/CategoriaService.java
@@ -1,6 +1,7 @@
 package com.smanager.sales_management.service;
 
 import com.smanager.sales_management.entity.Categoria;
+import com.smanager.sales_management.exception.BusinessRuleException;
 import com.smanager.sales_management.repository.CategoriaRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,13 +26,20 @@ public class CategoriaService {
     }
 
     public Categoria save(Categoria categoria) {
+        validDuplicateCategoria(categoria);
         return categoriaRepository.save(categoria);
     }
 
     public Categoria update(Long codigo, Categoria categoria) {
         Categoria categoriaSave = validCategoria(codigo);
+        validDuplicateCategoria(categoria);
         BeanUtils.copyProperties(categoria, categoriaSave, "codigo");
         return categoriaRepository.save(categoriaSave);
+    }
+
+    public void delete(Long codigo) {
+        Categoria categoria = validCategoria(codigo);
+        categoriaRepository.delete(categoria);
     }
 
     private Categoria validCategoria(Long codigo) {
@@ -40,5 +48,12 @@ public class CategoriaService {
             throw new EmptyResultDataAccessException(1);
         }
         return categoria.get();
+    }
+
+    private void validDuplicateCategoria(Categoria categoria) throws BusinessRuleException {
+        Categoria categoriaDuplicate = categoriaRepository.findByName(categoria.getName());
+        if (categoriaDuplicate != null && !categoriaDuplicate.getCodigo().equals(categoria.getCodigo())) {
+            throw new BusinessRuleException(String.format("A categoria %s já está cadastrada", categoria.getName().toUpperCase()));
+        }
     }
 }
