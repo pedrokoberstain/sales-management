6f39d4819f6b1f63a364fc52d0470947675c1f36
pedro <pedrokoberstain2@gmail.com>
2024-08-15 01:27:28 -0400

    feat: Criando a exceção para as essa regra de negócio que foi buscar por nome

---
+++ src/main/java/com/smanager/sales_management/exception/BusinessRuleException.java
@@ -0,0 +1,8 @@
+package com.smanager.sales_management.exception;
+
+public class BusinessRuleException extends RuntimeException {
+
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
